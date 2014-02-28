30d5550ece6637bc720736778f9382dd8a2807e0
Rob <robthomp@uw.edu>
2014-02-27 18:58:47 -0800

    Forgot some fields in the section layer

--- data/section.php
+++ data/section.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect("master.cmu4mm2fobzj.us-west-2.rds.amazonaws.com","habbal","Dp6wNcah47awWWbL","cbp_main",3306) or die("Error " . mysqli_error($link));
 
-$query = "SELECT comp.charMatch, comp.gaps, comp.SWalign, comp.docB, comp.docAstart, comp.docAend, comp.docBstart, comp.docBend, comp.differencesA, comp.differencesB, comp.compLabel, bills.IntrDate, bills.Party FROM bills_sectcomp as comp, bills_sections as sec, bills WHERE comp.idA = " . $_GET["section"] . " AND sec.Bill_id = bills.id AND comp.idB = sec.sec_id AND NOT comp.docB like '%hr3590%'" or die("Error in the consult.." . mysqli_error($link));
+$query = "SELECT comp.charMatch, comp.gaps, comp.SWalign, comp.docB, comp.textA, comp.textB, comp.docAstart, comp.docAend, comp.docBstart, comp.docBend, comp.differencesA, comp.differencesB, comp.compLabel, bills.IntrDate, bills.Party, tex.text as sectionText, bills.URL FROM bills_sectcomp as comp, bills_sections as sec, bills, bills_secttext as tex WHERE comp.idA = " . $_GET["section"] . " AND sec.Bill_id = bills.id AND comp.idB = sec.sec_id AND tex.sec_id = idA AND NOT comp.docB like '%hr3590%'" or die("Error in the consult.." . mysqli_error($link));
 
 $result = $link->query($query);
 
