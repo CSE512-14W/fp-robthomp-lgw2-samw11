2d52d897f6e80c38211d72e2f98487fc170f7b83
Rob <robthomp@uw.edu>
2014-03-01 14:42:58 -0800

    Layer 1 data should be finalized and both data scripts are updated

--- data/connect.php
+++ data/connect.php
@@ -1,20 +1,33 @@
 <?php
 $link = mysqli_connect("master.cmu4mm2fobzj.us-west-2.rds.amazonaws.com","habbal","Dp6wNcah47awWWbL","cbp_main",3306) or die("Error " . mysqli_error($link));
 
-$query = "SELECT comp.idA, bills.IntrDate, bills.Party FROM bills_sectcomp as comp, bills_sections as sec, bills WHERE sec.Bill_id = bills.id AND comp.idB = sec.sec_id AND comp.docA = 'hr3590-111-enr' AND NOT comp.docB like '%hr3590%' ORDER BY idA" or die("Error in the consult.." . mysqli_error($link));
+$query = "(SELECT comp.idA as secID, bills.IntrDate, bills.Party FROM bills_sectcomp as comp, bills_sections as sect, bills WHERE comp.idB = sect.sec_id AND sect.Bill_id = bills.id AND comp.docA = 'hr3590-111-enr' AND NOT comp.docB like 'hr3590%' AND comp.SWalign > 1.0 AND comp.charMatch > 200 AND (comp.propA > 0.7 OR comp.propB > 0.7)) UNION (SELECT comp.idB as secID, bills.IntrDate, bills.Party FROM bills_sectcomp as comp, bills_sections as sect, bills WHERE comp.idA = sect.sec_id AND sect.Bill_id = bills.id AND comp.docB = 'hr3590-111-enr' AND NOT comp.docA like 'hr3590%' AND comp.SWalign > 1.0 AND comp.charMatch > 200 AND (comp.propA > 0.7 OR comp.propB > 0.7))" or die("Error in the consult.." . mysqli_error($link));
+
+#(SELECT comp.idA as secID, bills.IntrDate, bills.Party FROM bills_sectcomp as comp, bills_sections as sect, bills WHERE comp.idB = sect.sec_id AND sect.Bill_id = bills.id AND comp.docA = 'hr3590-111-enr' AND NOT comp.docB like 'hr3590%' AND comp.SWalign > 1.0 AND comp.charMatch > 200 AND (comp.propA > 0.7 OR comp.propB > 0.7)) UNION (SELECT comp.idB as secID, bills.IntrDate, bills.Party FROM bills_sectcomp as comp, bills_sections as sect, bills WHERE comp.idA = sect.sec_id AND sect.Bill_id = bills.id AND comp.docB = 'hr3590-111-enr' AND NOT comp.docB like 'hr3590%' AND comp.SWalign > 1.0 AND comp.charMatch > 200 AND (comp.propA > 0.7 OR comp.propB > 0.7))
+
 
 $result = $link->query($query);
 
 $out = array();
-
-$row = mysqli_fetch_array($result);
-$count = 1;
-$secId = $row["idA"];
-$minDate = strtotime($row["IntrDate"]);
-$minParty = $row["Party"];
+$sections = array();
 
 while($row = mysqli_fetch_array($result)) {
-	if ($secId != $row["idA"]) {
+	if (!array_key_exists($row["secID"], $sections)) {
+		$arr = array();
+		$arr["secID"] = $row["secID"];
+		$arr["matchingBills"] = 1;
+		$arr["minDate"] = strtotime($row["IntrDate"]);
+		$arr["party"] = $row["Party"];
+		$sections[$row["secID"]] = $arr;
+	} else {
+		$sections[$row["secID"]]["matchingBills"]++;
+		if ($sections[$row["secID"]]["minDate"] > strtotime($row["IntrDate"])) {
+			$sections[$row["secID"]]["minDate"] = strtotime($row["IntrDate"]);
+			$sections[$row["secID"]]["party"] = $row["Party"];
+		}
+	}
+	
+	/*if ($secId != $row["secId"]) {
 		$newRow = array();
 		$newRow["secId"] = $secId;
 		$newRow["party"] = $minParty;
@@ -23,7 +36,7 @@ while($row = mysqli_fetch_array($result)) {
 		$out[] = $newRow;
 		
 		$count = 1;
-		$secId = $row["idA"];
+		$secId = $row["secId"];
 		$minDate = strtotime($row["IntrDate"]);
 		$minParty = $row["Party"];
 	} else {
@@ -33,15 +46,12 @@ while($row = mysqli_fetch_array($result)) {
 			$minDate = $tempDate;
 			$minParty = $row["Party"];
 		}
-	}
+	}*/
 }
 
-$newRow = array();
-$newRow["secId"] = $secId;
-$newRow["party"] = $minParty;
-$newRow["matchingBills"] = $count;
-$newRow["minDate"] = $minDate;
-$out[] = $newRow;
+foreach ($sections as $section) {
+	$out[] = $section;
+}
 
 print(json_encode($out));
 
--- data/section.php
+++ data/section.php
@@ -1,15 +1,40 @@
 <?php
 $link = mysqli_connect("master.cmu4mm2fobzj.us-west-2.rds.amazonaws.com","habbal","Dp6wNcah47awWWbL","cbp_main",3306) or die("Error " . mysqli_error($link));
 
-$query = "SELECT comp.charMatch, comp.gaps, comp.SWalign, comp.docB, comp.textA, comp.textB, comp.docAstart, comp.docAend, comp.docBstart, comp.docBend, comp.differencesA, comp.differencesB, comp.compLabel, bills.IntrDate, bills.Party, tex.text as sectionText, bills.URL FROM bills_sectcomp as comp, bills_sections as sec, bills, bills_secttext as tex WHERE comp.idA = " . $_GET["section"] . " AND sec.Bill_id = bills.id AND comp.idB = sec.sec_id AND tex.sec_id = idA AND NOT comp.docB like '%hr3590%'" or die("Error in the consult.." . mysqli_error($link));
+$query = "SELECT comp.charMatch, comp.gaps, comp.SWalign, comp.docB, comp.textA, comp.textB, comp.docAstart, comp.docAend, comp.docBstart, comp.docBend, comp.differencesA, comp.differencesB, comp.compLabel, bills.IntrDate, bills.Party, bills.URL FROM bills_sectcomp as comp, bills_sections as sec, bills WHERE comp.idA = " . $_GET["section"] . " AND sec.Bill_id = bills.id AND comp.idB = sec.sec_id AND NOT comp.docB like '%hr3590%'" or die("Error in the consult.." . mysqli_error($link));
 
 $result = $link->query($query);
 
 $out = array();
+$rows = array();
 
 while($row = mysqli_fetch_array($result)) {
-	$out[] = $row;
+	$arr = array();
+	$arr["charMatch"] = $row["charMatch"];
+	$arr["gaps"] = $row["gaps"];
+	$arr["SWalign"] = $row["SWalign"];
+	$arr["docB"] = $row["docB"];
+	$arr["textA"] = $row["textA"];
+	$arr["textB"] = $row["textB"];
+	$arr["docAstart"] = $row["docAstart"];
+	$arr["docBstart"] = $row["docBstart"];
+	$arr["docAend"] = $row["docAend"];
+	$arr["docBend"] = $row["docBend"];
+	$arr["differencesA"] = $row["differencesB"];
+	$arr["differencesB"] = $row["differencesB"];
+	$arr["compLabel"] = $row["compLabel"];
+	$arr["IntrDate"] = $row["IntrDate"];
+	$arr["Party"] = $row["Party"];
+	$arr["URL"] = $row["URL"];
+	$rows[] = $arr;
 }
+
+$out["matches"] = $rows;
+
+$query = "SELECT text FROM bills_secttext WHERE sec_id = " . $_GET["section"];
+$result = $link->query($query);
+$row = mysqli_fetch_array($result);
+$out["sectionText"] = $row["text"];
 
 print(json_encode($out));
 
